788712eef752e20ce09933e1ee6b53be789275ed
Roy <roykuijper1997@hotmail.com>
2017-05-16 08:00:50 +0000

    lol

--- resources/views/aanmelden/aanmelden.blade.php
+++ resources/views/aanmelden/aanmelden.blade.php
@@ -116,7 +116,7 @@ Beschikbare Sloten:
             @foreach($slotenVoorkeur as $slotVoorkeur)
             @if($slotVoorkeur->dag == "Vrijdag")
             <tr>
-                <td><input type="radio" name="slotvoorkeur" value="{{ $slotvoorkeur->id }}"></td>
+                <td><input type="radio" name="slotvoorkeur" value="{{ $slotVoorkeur->id }}"></td>
                 <td>{{ $slotVoorkeur->dag }}</td>
                 <td>{{ $slotVoorkeur->beginTijd }} </td>
                 <td>{{ $slotVoorkeur->eindTijd }}</td>
@@ -146,7 +146,7 @@ Beschikbare Sloten:
             @foreach($slotenVoorkeur as $slotVoorkeur)
             @if($slotVoorkeur->dag == "Zondag")
             <tr>
-                <td><input type="radio" name="slotvoorkeur" value="{{ $slotvoorkeur->id }}"></td>
+                <td><input type="radio" name="slotvoorkeur" value="{{ $slotVoorkeur->id }}"></td>
                 <td>{{ $slotVoorkeur->dag }}</td>
                 <td>{{ $slotVoorkeur->beginTijd }} </td>
                 <td>{{ $slotVoorkeur->eindTijd }}</td>
