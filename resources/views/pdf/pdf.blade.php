998ff19c4fdcbf684d58ad799ada6e32de3b5417
Roy <roykuijper1997@hotmail.com>
2017-05-22 18:04:46 +0000

    lol

--- resources/views/pdf/pdf.blade.php
+++ resources/views/pdf/pdf.blade.php
@@ -1,16 +1,11 @@
 <center><h2>E-Tickets</h2></center><br>
 
-            <tr>
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <td></td>
-            </tr>
-
 @foreach($ticketarray as $ticket)
-    <center><h3>QR-code Ticket</h3></center>
-    <center><img src="http://conferentie-justsomeguy.c9users.io/src/tickets/{{ $ticket->id }}.jpg"></center><br>
+    <h3>QR-code Ticket</h3>
+    <img src="/src/tickets/{{ $ticket->id }}.jpg"><br>
 @endforeach
 <br>
 @foreach($maaltijdarray as $maaltijd)
-    <center><h3>QR-code Maaltijd</h3></center>
-    <center><img style="align:center;" src="http://conferentie-justsomeguy.c9users.io/src/maaltijden/{{ $maaltijd->id }}.jpg"></center><br>
+    <h3>QR-code Maaltijd</h3>
+    <img style="align:center;" src="/src/maaltijden/{{ $maaltijd->id }}.jpg"><br>
 @endforeach
